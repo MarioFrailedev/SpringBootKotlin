2b6c12df398576d7f261f9274f97740018adf867
Mario Fraile <mariofrailedev@gmail.com>
2019-06-26 16:30:27 -0500

    kotlin features

---
+++ KotlinSpringDemo/src/main/kotlin/com/manidevs/KotlinSpringDemo/TestingCodeKotlin/Pet.kt
@@ -0,0 +1,3 @@
+package com.manidevs.KotlinSpringDemo.TestingCodeKotlin
+
+data class Pet (val name: String, val age:Int, val ownerName: String)
---
+++ KotlinSpringDemo/src/main/kotlin/com/manidevs/KotlinSpringDemo/TestingCodeKotlin/TestingKotling.kt
@@ -0,0 +1,6 @@
+package com.manidevs.KotlinSpringDemo.TestingCodeKotlin
+
+fun main(args:Array<String>){
+    var who :String? = "Meeseeks"
+    println("I'm Mr.$who lock at mee!!!")
+}
